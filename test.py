eadfa55b2849b138d626c1c66afe8169203c772c
YamaU10622 <tokuyama.yudai.jp@alumni.tsukuba.ac.jp>
2024-12-11 15:04:41 +0900

    test.py 改修

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-from IPython.display import HTML
-HTML("test")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render の PORT 環境変数を使用
+    app.run(host="0.0.0.0", port=port)
